09a5b5646b4485667ac8409682cd4e8a8a03f305
Cristiano Soares <crssoares.js@gmail.com>
2020-04-23 23:16:56 -0300

    root import ~/

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-import AuthLayout from '../pages/_layouts/auth/index';
-import DefaultLayout from '../pages/_layouts/default/index';
+import AuthLayout from '~/pages/_layouts/auth/index';
+import DefaultLayout from '~/pages/_layouts/default/index';
 
 export default function RouteWrapper({
   component: Component,

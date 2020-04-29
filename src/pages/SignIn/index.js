213bf7e23e45945b73d1539752a79d46a61e1dd7
Cristiano Soares <crssoares.js@gmail.com>
2020-04-29 20:10:35 -0300

    persisting data

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -25,8 +25,6 @@ export default function SignIn() {
     },
     validationSchema: schema,
     onSubmit: (values) => {
-      console.tron.log(values);
-
       dispatch(signInRequest(values.email, values.password));
     },
   });
